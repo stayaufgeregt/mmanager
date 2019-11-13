9c6b255bdedd8004fbff303bc5b5a0645fc3274a
Samer <samer.abou-jaoude@telecomnancy.eu>
2019-11-13 21:46:38 +0100

    add default downloader config if file is missing

--- download.py
+++ download.py
@@ -1,11 +1,18 @@
 import youtube_dl as yt
 import json
+import os.path
 
+DEFAULT_CONFIG="""{"noplaylist": 1, "format": "m4a","default_search": "ytsearch1", "musicdir": "../music/", "outtmpl": "../music/%(title)s.%(ext)s", "nocheckcertificate": false, "extractaudio": 1}"""
+			
 class Downloader:
 
 
 	def __init__(self):
 		
+		if not(os.path.exists("./resources/downloaderConfig")):
+			with open("./resources/downloaderConfig",'w') as cfg:
+				cfg.write(DEFAULT_CONFIG)
+				
 		with open("./resources/downloaderConfig",'r') as config_file:
 			dict_config=json.load(config_file)
 			self.downloader=yt.YoutubeDL(dict_config)	
